e9f387e5a895e5f1b8f558f783d6735ab4ff70d2
Yuvant Besre <yuvisingh121212@gmail.com>
2021-01-15 13:26:12 +0530

    complete Portfolio

--- Projects/models.py
+++ Projects/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Project(models.Model):
     Image = models.ImageField(upload_to='images/')
-    Summary = models.CharField(max_length=255)
+    Summary = models.CharField(max_length=1000)
+    GitHub = models.URLField(blank=True)
 
     def __str__(self):
         return self.Summary
